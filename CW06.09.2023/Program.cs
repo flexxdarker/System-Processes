94d761bb96773f170e24c0a31bca6ff1d8d43f9e
flexxdarker <ksenzhakmax@gmail.com>
2023-09-06 19:08:33 +0300

    Add project files.

---
+++ CW06.09.2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CW06._09._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
