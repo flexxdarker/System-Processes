e6d489329bf7e08c39c3c7cdd5d8202de4667db3
flexxdarker <ksenzhakmax@gmail.com>
2023-09-08 20:55:55 +0300

    cw08.09.2023

---
+++ Cw08.09.2023/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Cw08._09._2023
+{
+    internal class Program
+    {
+        public static class Result
+        {
+            public static int Words = 0;
+            public static int Lines = 0;
+            public static int Punctuations = 0;
+           
+        }
+        public static void Counts(object text)
+        {
+            string text2 = Convert.ToString(text);
+            for (int i = 0; i < text2.Length; i++)
+            {
+                if (text2[i] == ' ')
+                {
+                    Interlocked.Increment(ref Result.Words);
+                }
+            }
+            Console.WriteLine($"\tWords: {Result.Words}");
+            Result.Lines = text2.Split('\n').Length;
+            Console.WriteLine($"Lines: {Result.Lines}");
+            for (int i = 0; i < text2.Length; i++)
+            {
+                if (text2[i] == '!' || text2[i] == ';' || text2[i] == ',' ||
+                   text2[i] == ':' || text2[i] == '?' || text2[i] == '.')
+                {
+                    Interlocked.Increment(ref Result.Punctuations);
+                }
+            }
+            Console.WriteLine($"Punctuations: {Result.Punctuations}");
+        }
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("C:\\Users\\dev\\source\\repos\\System Processes\\Cw08.09.2023");
+            foreach(string file in files)
+            {
+                string text = File.ReadAllText(file);
+                Thread thread = new Thread(Counts);
+                thread.Start(text);
+            }
+        }
+    }
+}
